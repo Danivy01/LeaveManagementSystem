7fcd8710f064e32b77da66eb7eb5db9696d314b2
Yvette Rimas <daniellayvetterimas@gmail.com>
2026-07-16 11:13:40 +0800

    feature: add leave model

---
+++ LeaveManagementSystem/Models/Leave.cs
@@ -0,0 +1,8 @@
+﻿namespace LeaveManagementSystem.Models {
+    public class Leave {
+        public int ID { get; set; }
+        public string EmployeeName { get; set; }
+        public string LeaveType { get; set; }
+        public string Status { get; set; }
+    }
+}
