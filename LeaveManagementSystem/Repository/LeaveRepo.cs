8eaabadec34429d7469c3e4d673c08c90d40a409
Yvette Rimas <daniellayvetterimas@gmail.com>
2026-07-17 15:01:17 +0800

    feat: implement leave repository

---
+++ LeaveManagementSystem/Repository/LeaveRepo.cs
@@ -0,0 +1,15 @@
+﻿using LeaveManagementSystem.Interfaces;
+using LeaveManagementSystem.Models;
+
+namespace LeaveManagementSystem.Repository {
+    public class LeaveRepo : ILeaveRepo {
+        private readonly List<Leave> leaves = [
+            new Leave{ ID = 1, EmployeeName = "Daniella Yvette Rimas", LeaveType = "Sick", Status = "Pending"},
+            new Leave{ ID = 2, EmployeeName = "John Doe Cruz", LeaveType = "Sick", Status = "Pending"},
+            new Leave{ ID = 3, EmployeeName = "Danivy Maris Jasmin", LeaveType = "Vacation Leave", Status = "Approved"}
+        ];
+        public List<Leave> GetLeaveList() {
+            return leaves;
+        }
+    }
+}
