fd3be9ccc6d7c6309502a652781e351708327a76
Yvette Rimas <daniellayvetterimas@gmail.com>
2026-07-22 13:43:12 +0800

    feat: add required data annotation to models

--- LeaveManagementSystem/Models/Leave.cs
+++ LeaveManagementSystem/Models/Leave.cs
@@ -1,8 +1,14 @@
-﻿namespace LeaveManagementSystem.Models {
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LeaveManagementSystem.Models {
     public class Leave {
         public int ID { get; set; }
+
+        [Required]
         public string EmployeeName { get; set; }
+        [Required]
         public string LeaveType { get; set; }
+
         public string Status { get; set; }
     }
 }
